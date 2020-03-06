71fd883209f012b4919d0e8fefb136e0331b674a
L Fahn-Lai <l.fahnlai@gmail.com>
2020-03-05 20:07:48 -0500

    added 'Using OpenCV.ipynb'

--- scanDir.py
+++ scanDir.py
@@ -1,16 +1,18 @@
 import os
 import re
         
-def scanDir(directory, extension='avi', verbose=False):
-    vid_list=[]
+def scanDir(directory, extension='avi', filter_string=None, verbose=False):
+    file_list=[]
     for root, dirs, files in os.walk(directory):
         for name in files:
             if name.lower().endswith(extension):
                 filename = os.path.join(root, name)
                 if verbose == True:
                     print("Found file with extension ."+ extension + ": " + filename)
-                vid_list.append(filename)
+                file_list.append(filename)
                 continue
             else:
                 continue
-    return(vid_list)
+    if filter_string != None:
+        file_list = [file for file in file_list if not re.search(filter_string, file)]
+    return(file_list)
